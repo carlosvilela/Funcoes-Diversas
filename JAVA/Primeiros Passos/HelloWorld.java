ad07e8bf388cc11727b01c26c7dc81afaf944556
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-01-04 22:45:32 -0200

    Create HelloWorld.java

---
+++ JAVA/Primeiros Passos/HelloWorld.java
@@ -0,0 +1,35 @@
+/**
+ O primeiro passo é criar a classe, sabendo que cada classe que você criar,
+na compilação, será criado um arquivo com o nome da classe, como, por exemplo
+HelloWorld.class e OlaMundo.class, sendo cada classe um executavel distinto
+que pode ou não ser usado por outros programas em java.
+ Em seguida deve criar a função principal que é a função que será chamada automaticamente,
+conhecida como MAIN, esta é a função principal, dentro dela contem o codigo a ser definido,
+esta função deve ser criada usando o modelo padrão: public static void main(String args[]).
+ **/
+
+
+// Classe HelloWorld
+
+class HelloWorld{
+	
+	public static void main(String args[]){
+		
+		System.out.println("Hello World !");
+		
+	}
+	
+}
+
+// Classe OlaMundo
+
+class OlaMundo{
+	
+	public static void main (String args[]){
+	
+	System.out.println("Ola Mundo !");
+		
+	}
+	
+
+}
