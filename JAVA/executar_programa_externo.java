824ac0e1513d9bb2fe6b4fa9edbf100bb779f324
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-01-19 20:53:09 -0200

    Create executar_programa_externo.java

---
+++ JAVA/executar_programa_externo.java
@@ -0,0 +1,16 @@
+class test{
+	
+	public static void main (String args[]){
+		
+		try {
+			
+			
+			Process executar = Runtime.getRuntime().exec("firefox");
+			
+		}catch(Exception e){
+			
+		}
+		
+	}
+	
+}
