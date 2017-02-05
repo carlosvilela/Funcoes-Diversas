c1f2d32b68846bdecef99994f12298163809a959
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-02-05 09:09:21 -0200

    Create mostrar_Banco_de_Dados.sql

---
+++ MySQL/mostrar_Banco_de_Dados.sql
@@ -0,0 +1,9 @@
+/*
+O comando SHOW mostra a relação de Banco de Dados ou Tabelas,
+para mostrar a relação de banco de dados digite SHOW DATABASES,
+ou seja, mostrar Bonco de Dados (show databases), nunca poderá
+esquecer o ponto e virgula que deve ser usado para
+finalizar o bloco de comandos.
+*/
+
+SHOW DATABASES;
