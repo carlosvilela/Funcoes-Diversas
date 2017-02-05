c3602ed2412b29a60989213e9d73438c5e45b248
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-02-05 09:14:07 -0200

    Create criar_Banco_de_Dados.sql

---
+++ MySQL/criar_Banco_de_Dados.sql
@@ -0,0 +1,10 @@
+/*
+Para criar um banco de dados, basta inserir o seguinte comando:
+CREATE DATABASE <nome>; lembrando de nunca esquecer do ponto e
+virgula para fechar a linha de comando.
+este comando significa Criar base de dado <nome>, se perceber,
+usa-se o database no singular, diferentemente de quando queremos mostrar
+os banco de dados, onde usamos Show Databases, sendo database no plural, databases.
+*/
+
+CREATE DATABASE testando;
