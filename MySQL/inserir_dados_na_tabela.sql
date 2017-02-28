f9bb8b2d7609292c572fab7df38f79ea48122801
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-02-28 17:51:38 -0300

    Create inserir_dados_na_tabela.sql

---
+++ MySQL/inserir_dados_na_tabela.sql
@@ -0,0 +1,6 @@
+/*
+Para inserir os dados em uma tabela basta utilizar o seguinte comando:
+INSERT INTO <nomeTabela> (<nomeCampo>, <nomeCampo> ...) VALUES (<valoresCampo>, <valoresCampo) ...);
+em outras palavras: Insirir Dentro da Tabela (nos campos) os Valores (valores dos campos)
+*/
+INSERT INTO aluno (nome, sobrenome, idade) VALUES ('Carlos', 'Vilela', '30');
