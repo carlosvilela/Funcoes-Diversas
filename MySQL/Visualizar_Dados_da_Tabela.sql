4b7124a68b30a210a83772624fc37db6b030f817
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-02-28 17:55:15 -0300

    Create Visualizar_Dados_da_Tabela.sql

---
+++ MySQL/Visualizar_Dados_da_Tabela.sql
@@ -0,0 +1,9 @@
+/*
+Para visualizar os dados dentro de uma tabela basta utilizar o seguinte comando:
+SELECT <CampoTabela> FROM <Tabela>
+Em outras palavras: Selecione o Campo <CampoTabela> da <Tabela>
+*/
+
+SELECT * FROM aluno2; // mostra todos os campos
+// ou
+SELECT nome FROM aluno2; // mostra apenas o campo <nome>
