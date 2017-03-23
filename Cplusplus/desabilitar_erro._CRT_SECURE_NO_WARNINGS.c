e0f32016cfda9f6dd9a88f3e569ade2f0f97ca36
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-03-23 07:29:32 -0300

    Create desabilitar_erro._CRT_SECURE_NO_WARNINGS.c

---
+++ Cplusplus/desabilitar_erro._CRT_SECURE_NO_WARNINGS.c
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+
+#include <Cstdio>
+#include <Cstdlib>
+
+#pragma warning(disable:4996) // desabilitar erro _CRT_SECURE_NO_WARNINGS
+
+
+int main(int argc, char** argv[]){
+
+	int x;
+	
+	printf("ooiii");
+
+	scanf("%d", &x);
+
+	system("pause >> null");
+
+	return 0;
+
+
+}
