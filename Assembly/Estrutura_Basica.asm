2e6c89a7f0382c5c67361631fbcb2a161bbfa5c9
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-04-15 20:03:43 -0300

    Create Estrutura_Basica.asm

---
+++ Assembly/Estrutura_Basica.asm
@@ -0,0 +1,17 @@
+;http://kipirvine.com/asm/gettingStartedVS2013/index.htm
+; AddTwo.asm - adds two 32-bit integers.
+; Chapter 3 example
+
+.386
+.model flat,stdcall
+.stack 4096
+ExitProcess proto,dwExitCode:dword
+
+.code
+main proc
+	mov	eax,5				
+	add	eax,6				
+
+	;invoke ExitProcess,0
+main endp
+end main
