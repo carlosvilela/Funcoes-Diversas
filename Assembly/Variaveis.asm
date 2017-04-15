cf5734245dab3f3dd52ece0bafb11005418e7ccf
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-04-15 19:47:13 -0300

    Create Variaveis.asm

---
+++ Assembly/Variaveis.asm
@@ -0,0 +1,29 @@
+.486
+.model small
+
+.data
+
+caracter	db	'a' ; 
+
+conjuntoCaracter db 'Carlos Vilela', 0Ah
+
+numeroGrande1 dq 123456789
+
+numeroGrande2 dq 553210.853216
+
+numeroEnorme dt 1234567894567897897
+
+numeroEnorme2 dt 321654987.321654654654
+
+.code
+start:
+
+
+end start
+
+;Directive	Purpose	Storage Space
+;DB	Define Byte	allocates 1 byte
+;DW	Define Word	allocates 2 bytes
+;DD	Define Doubleword	allocates 4 bytes
+;DQ	Define Quadword	allocates 8 bytes
+;DT	Define Ten Bytes	allocates 10 bytes
