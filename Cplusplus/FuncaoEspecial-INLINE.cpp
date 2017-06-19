307fe12f4ddd75e494a9d71dab1b96991935be30
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-06-19 14:25:34 -0300

    Create FuncaoEspecial-INLINE.cpp

---
+++ Cplusplus/FuncaoEspecial-INLINE.cpp
@@ -0,0 +1,20 @@
+inline int soma (int a, int b){
+    int r;
+    
+    r = a + b;
+    
+    return r;
+}
+
+main (){
+    
+    int resultado;
+    
+    resultado = soma (1,5);
+    printf("Resultado = %d", resultado);
+ 
+    resultado = soma (8,3);
+    printf("\nResultado = %d", resultado);
+    
+    
+}
