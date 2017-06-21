b23bb72144729e80b2230cf047ed811488424b4e
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-06-21 14:21:15 -0300

    Create COUT.PUT-saida-caracter.cpp

---
+++ Cplusplus/COUT.PUT-saida-caracter.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+    
+    int i = 65;
+    char a = char(i); // converte o i e retorna o caracter referente a tabela ASCII correspondente ao numero decimal de i
+    char b = 'W';
+    
+    cout.put(i); // retorna o caracter referente a tabela ASCII correspondente ao numero decimal de i
+    cout << endl;
+    cout.put(a); // retorna o caracter referente a tabela ASCII correspondente ao numero decimal de i
+    cout << endl;
+    cout.put(b); // retorna o caracter contido na variavel b
+    
+}
