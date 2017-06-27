6a9e6a498afc03b0100265be53fe737213dfadfc
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-06-27 11:22:51 -0300

    Update OUTLINE-abrir-arquivo-em-modo-saida-acrescimo.cpp

--- Cplusplus/mexendo-com-arquivos/OUTLINE-abrir-arquivo-em-modo-saida-acrescimo.cpp
+++ Cplusplus/mexendo-com-arquivos/OUTLINE-abrir-arquivo-em-modo-saida-acrescimo.cpp
@@ -11,7 +11,10 @@ ofstream outline(NomeArquivo, ios::out); // cria ou abre aruivo em modo saida / 
   
   /*
   Você usa a constante de enumeração ios::out para abrir o arquivo no modo de saída. Para abrir o arquivo no modo de acréscimo, use o ios::app.
-  */
+
+Quando você abre um arquivo no modo de saída, todos os dados armazenados nesse arquivo são descartados. Caso deseje fazer uma adição a um arquivo existente, você precisa abrir o arquivo no modo de acréscimo. Os dados serão então adicionados ao final do arquivo. 
+ 
+ */
 
 
 }
