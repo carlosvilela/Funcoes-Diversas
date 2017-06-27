eb7cdd52a55f89b927cd3869be6363fba7bdc52f
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-06-27 11:17:17 -0300

    Create INFILE-abrir-arquivo-modo-entrada-acrescimo.cpp

---
+++ Cplusplus/mexendo-com-arquivos/INFILE-abrir-arquivo-modo-entrada-acrescimo.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+
+using namespace std;
+
+
+const char* NomeArquivo = "exemplo.txt";
+
+main(){
+
+ifstream infile(NomeArquivo, ios::in); // pega arquivo existente e abre ele em modo de entrada de dados
+
+
+}
