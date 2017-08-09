1081f048767a746a9d733c8f9e46830a3eea808c
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-08-09 10:28:47 -0300

    Create CondicionalTernario.c

---
+++ C/CondicionalTernario.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+main(){
+
+int a, b;
+a = 10;
+b = 11;
+
+a >= b? printf("a e maior ou igual a b") : printf("a nao e maior ou igual a b"); // nao precisa usar o if -  condicional ternario
+
+
+}
