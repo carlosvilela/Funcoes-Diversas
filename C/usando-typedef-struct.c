5a27272b4b6089b6c6ba83a64c9c74e717ec1a94
Carlos Vilela <carlosvilela@users.noreply.github.com>
2017-09-14 09:51:02 -0300

    Create usando-typedef-struct.c

---
+++ C/usando-typedef-struct.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+typedef struct {
+	
+	int dia;
+	int mes;
+	int ano;
+	
+} Data;
+
+typedef struct{
+	
+	char nome[80];
+	
+	Data aniversario;
+	
+} Contato;
+
+main(){
+
+
+}
